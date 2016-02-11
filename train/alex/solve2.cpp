342a0d28308ab1d89caebebab3037c52e14995ab
Alex Auvolat <alex@adnab.me>
2016-02-11 17:28:22 +0100

    New parallelism

--- train/alex/solve2.cpp
+++ train/alex/solve2.cpp
@@ -1,5 +1,7 @@
 #include <utility>
 #include <future>
+#include <thread>
+#include <mutex>
 #include <chrono>
 
 #include "main.hpp"
@@ -87,14 +89,14 @@ vector<string> solve2_int(const vector< vector<bool> > &data, int p1, int p2) {
 	// generate all possible commands
 	for (int i = 0; i < L; i++) {
 		for (int j = 0; j < C; j++) {
-			for (int r = 0; r < 5; r++) {
+			for (int r = 0; r < 6; r++) {
 				if (i - r >= 0 && j - r >= 0 && i + r < L && j + r < C)
 					all_cmds.push_back(new square(i, j, r));
 			}
-			for (int i2 = i+1; i2 < min(i+10, L); i2++) {
+			for (int i2 = i+1; i2 < min(i+15, L); i2++) {
 				all_cmds.push_back(new line(i, j, i2, j));
 			}
-			for (int j2 = j+1; j2 < min(j+10, C); j2++) {
+			for (int j2 = j+1; j2 < min(j+15, C); j2++) {
 				all_cmds.push_back(new line(i, j, i, j2));
 			}
 		}
@@ -138,25 +140,53 @@ vector<string> solve2_int(const vector< vector<bool> > &data, int p1, int p2) {
 				commands.push_back(strfmt("ERASE_CELL %d %d", i, j));
 		}
 	}
+	fprintf(stderr, "(%d %d -> %d)", p1, p2, commands.size());
 
 	return commands;
 }
 
 vector<string> solve2(const vector< vector<bool> > &data) {
+	
 
-	vector<future<vector<string>>> tasks;
+	vector<pair<int, int>> params;
+	mutex the_mutex;
+	vector<string> best_sol;
 
-	for (int p1 = 2; p1 < 4; p1 += 1) {
-		for (int p2 = 1; p2 < 8; p2++) {
-			tasks.push_back(async(launch::async, solve2_int, data, p1, p2));
+	for (int p1 = 2; p1 < 6; p1 += 1) {
+		for (int p2 = p1-1; p2 < 12; p2++) {
+			params.push_back(make_pair(p1, p2));
 		}
 	}
+	auto internal = [&]{
+			while(true) {
+				the_mutex.lock();
+				if (params.empty()) {
+					the_mutex.unlock();
+					break;
+				}
+				auto x = params.back();
+				params.pop_back();
+				the_mutex.unlock();
 
-	vector<string> sol;
-	for (int i = 0; i < tasks.size(); i++) {
-		auto x = tasks[i].get();
-		if (sol.empty() || x.size() < sol.size()) sol = x;
+				auto sol = solve2_int(data, x.first, x.second);
+
+				the_mutex.lock();
+				if (best_sol.empty() || sol.size() < best_sol.size()) {
+					fprintf(stderr, "New best: %d %d -> %d\n", x.first, x.second, sol.size());
+					best_sol = sol;
+				}
+				the_mutex.unlock();
+				}
+			};
+
+	vector<thread> threads;
+	for (int i = 0; i < 4; i++) {
+		threads.push_back(thread(internal));
 	}
 
-	return sol;
+	for (int i = 0; i < threads.size(); i++) {
+		threads[i].join();
+	}
+
+	return best_sol;
 }
